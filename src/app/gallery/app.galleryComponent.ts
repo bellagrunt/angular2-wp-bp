52370b800adc1d3636217aa1a14d0567acdd01a5
Matt Bellantoni <mattbellantoni3@gmail.com>
2017-02-21 14:58:28 -0500

    making changes to carousel and adding a gallery to gallery page.

--- src/app/gallery/app.galleryComponent.ts
+++ src/app/gallery/app.galleryComponent.ts
@@ -3,9 +3,25 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'gallery',
   templateUrl: './gallery.html',
-  styleUrls: ['./app.galleryComponent.css'],
+  styles: [`
+  	ul { padding:0; width:780px; margin:20px auto}
+  	li { display:inline;}
+        .tn{ 
+	   margin:2px 0px;
+	   box-shadow:#999 1px 1px 3px 1px; 
+	   cursor: pointer 
+        }
+  	.modal-content {
+	    width: 670px !important;
+	}
+  `]
 })
 export class galleryComponent { 
  
-
+   @Input() datasource;
+   selectedImage;
+ 
+   setSelectedImage(image){
+      this.selectedImage= image;	
+   }
 }
