d0d1a95229ce2c16be3e5cb9c84a93d549ad6528
Matt Bellantoni <mattbellantoni3@gmail.com>
2017-02-21 11:18:45 -0500

    working on gallery component to display images

--- src/app/gallery/app.galleryComponent.ts
+++ src/app/gallery/app.galleryComponent.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component ({
     templateUrl: './gallery.html',
+    styleUrls: ['./app.galleryComponent.css'],
 })
 
 export class galleryComponent {
-    
+
 }
