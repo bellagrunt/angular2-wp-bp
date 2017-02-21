97e0b6a9df85ab8ec41677951639c5ed241f2c0c
Matt Bellantoni <mattbellantoni3@gmail.com>
2017-02-21 14:28:13 -0500

    created carousel below the header

--- src/app/carousel/app.carouselComponent.ts
+++ src/app/carousel/app.carouselComponent.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
-
-@Component ({
-    templateUrl: './carousel.html',
+import {Component, Input} from '@angular/core';
+ 
+@Component({
+  selector: 'carousel',
+  templateUrl: './carousel.html',
+  styleUrls: ['./app.carouselComponent.css'],
 })
-
-export class carouselComponent {
-    
+export class carouselComponent { 
+ 
+   @Input() datasource;
+   selectedImage;
+ 
+   setSelectedImage(image){
+      this.selectedImage= image;	
+   }
 }
--- src/app/gallery/app.galleryComponent.ts
+++ src/app/gallery/app.galleryComponent.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
-@Component ({
-    templateUrl: './gallery.html',
-    styleUrls: ['./app.galleryComponent.css'],
+import {Component, Input} from '@angular/core';
+ 
+@Component({
+  selector: 'gallery',
+  templateUrl: './gallery.html',
+  styleUrls: ['./app.galleryComponent.css'],
 })
-
-export class galleryComponent {
+export class galleryComponent { 
+ 
 
 }
