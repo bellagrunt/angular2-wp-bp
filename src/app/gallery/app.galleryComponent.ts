4434fc26a70b330c40ef9ea7ed496b4e2da0b059
Matt Bellantoni <mattbellantoni3@gmail.com>
2017-02-27 14:45:37 -0500

    refactored gallery page to go directly to the gallery css and added a few styles.

--- src/app/gallery/app.galleryComponent.ts
+++ src/app/gallery/app.galleryComponent.ts
@@ -3,25 +3,15 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'gallery',
   templateUrl: './gallery.html',
-  styles: [`
-  	ul { padding:0; width:780px; margin:20px auto}
-  	li { display:inline;}
-        .tn{ 
-	   margin:2px 0px;
-	   box-shadow:#999 1px 1px 3px 1px; 
-	   cursor: pointer 
-        }
-  	.modal-content {
-	    width: 670px !important;
-	}
-  `]
+  styleUrls: ['./app.galleryComponent.css'],
+
 })
 export class GalleryComponent { 
  
-   @Input() datasource;
-   selectedImage;
+  //  @Input() datasource;
+  //  selectedImage;
  
-   setSelectedImage(image){
-      this.selectedImage= image;	
-   }
+  //  setSelectedImage(image){
+  //     this.selectedImage= image;	
+  //  }
 }
