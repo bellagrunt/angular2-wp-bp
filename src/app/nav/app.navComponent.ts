62cf9070ac88d1e49887150c5e29a813ba87dbca
Matt Bellantoni <mattbellantoni3@gmail.com>
2017-02-28 09:04:27 -0500

    changed nav bar to responsive with drop down

--- src/app/nav/app.navComponent.ts
+++ src/app/nav/app.navComponent.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nav',
+  selector: 'ct-navbar',
   templateUrl: `./nav.html`,
   styleUrls: ['./app.navComponent.css'],
 })
 export class NavComponent  { 
-
+    isIn = false;   // store state
+    toggleState() { // click handler
+        let bool = this.isIn;
+        this.isIn = bool === false ? true : false; 
+    }
 
 }
